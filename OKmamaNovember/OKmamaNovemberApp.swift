39a3283543edf14080b7a13cd7201ca09f237ea4
John <johnslaw09@gmail.com>
2020-10-24 14:31:13 -0500

    Initial Commit

---
+++ OKmamaNovember/OKmamaNovemberApp.swift
@@ -0,0 +1,17 @@
+//
+//  OKmamaNovemberApp.swift
+//  OKmamaNovember
+//
+//  Created by John Lawrence on 10/24/20.
+//
+
+import SwiftUI
+
+@main
+struct OKmamaNovemberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
