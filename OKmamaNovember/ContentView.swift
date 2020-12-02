b4f16955280973e7075ec5d414626a7e6c3ffb9f
John <johnslaw09@gmail.com>
2020-12-01 22:25:21 -0500

    Fixing audo Button

--- OKmamaNovember/ContentView.swift
+++ OKmamaNovember/ContentView.swift
@@ -6,16 +6,63 @@
 //
 
 import SwiftUI
+import AVFoundation
+import AVKit
 
 struct ContentView: View {
+//    var body: some View {
+//        Text("Hello, world!")
+    //            .padding()
+    //
+    @State var audioPlayer:AVAudioPlayer?
+    
+    @State var isPlaying : Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        VStack {
+            
+        
+        Button(action: {
+            if let path = Bundle.main.path(forResource: "1_3_", ofType: ".mp3"){
+                self.audioPlayer = AVAudioPlayer()
+                self.isPlaying.toggle()
+                
+                let url = URL(fileURLWithPath: path)
+                do {
+                    self.audioPlayer = try AVAudioPlayer(contentsOf: url)
+                    self.audioPlayer?.prepareToPlay()
+                    self.audioPlayer?.play()
+                    
+                } catch {
+                    print("Error")
+                }
+            
+            }
+        }, label: {
+            if isPlaying {
+                Image(systemName: "pause").font(Font.system(.largeTitle).bold())
+				// isPlaying = false;
+				
+            } else {
+                Image(systemName: "play.fill").font(Font.system(.largeTitle).bold())
+                
+            }
+            
+        }) // end button
+		.padding()
+            Text("Welcome to OK mama!")
+                .font(Font.custom("FredokaOne-Regular", size: 20))
+			
+		} // end VStack
+		
+        } // end View
+    
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+ 
